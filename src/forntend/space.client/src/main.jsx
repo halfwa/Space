c1dd52d1e853b7883d315357484d29d85204a54e
Yaroslav Shamov <yaroslav.shamov2@mail.ru>
2024-01-29 13:07:30 +0300

    add front base

---
+++ src/forntend/space.client/src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Home from './components/pages/home/Home.jsx'
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Home />
+  </React.StrictMode>,
+)
